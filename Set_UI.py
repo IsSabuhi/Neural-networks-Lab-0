5c031763a91b7caa257ced503a8ed7790aaa4753
IsSabuhi <sabuhi.israfilov2001@gmail.com>
2022-03-04 21:47:08 +0700

    change window

--- Set_UI.py
+++ Set_UI.py
@@ -1,11 +1,7 @@
 from tkinter import *
 from PIL import Image, ImageDraw
 import PIL
-
 from Recog import Recognize
-
-Cwidth = 80
-Cheight = 80
 
 
 class Window:
@@ -16,10 +12,13 @@ class Window:
         self.root = Tk()
         self.root.title(title)
         self.root.geometry(f"{width}x{height}")
+        self.white = (255, 255, 255)
+        self.Cwidth = 80
+        self.Cheight = 80
         self.left_frame = Frame(self.root)
-        self.right_frame = Frame(self.root, width=Cwidth, height=Cheight)
-        self.cv = Canvas(self.right_frame, width=Cwidth, height=Cheight, bg='white')
-        self.image = PIL.Image.new("RGB", (width, height))
+        self.right_frame = Frame(self.root, width=self.Cwidth, height=self.Cheight)
+        self.cv = Canvas(self.right_frame, width=self.Cwidth, height=self.Cheight, bg='white')
+        self.image = PIL.Image.new("RGB", (width, height), self.white)
         self.draw = ImageDraw.Draw(self.image)
         self.penSize_slider = 3
         self.buttonRecognize = Button(self.left_frame, text="Распознать", command=self.recognize_event, width=20)
@@ -29,13 +28,13 @@ class Window:
         self.buttonClear = Button(self.left_frame, text="Очистить", command=self.clear, width=20)
         self.lbl0 = Label(self.left_frame, text="Размер пера", font="Arial 10", width=15)
         self.lbl1 = Label(self.left_frame, text=" ", font="Arial 10", fg="black")
-        self.lbl2 = Label(self.left_frame, text=" ", font="Arial 9", width=15)
+        self.lbl2 = Label(self.left_frame, text=" ", font="Arial 9", width=20)
         if icon:
             self.root.iconbitmap(icon)
 
 
     def recognize_event(self):
-        self.rec.recognize(self.image)
+        self.rec.recognize(self.image, self.lbl1, self.lbl2, self.Cwidth, self.Cheight)
 
     def save_weights_event(self):
         self.rec.save_w()
@@ -47,6 +46,7 @@ class Window:
         self.right_frame.pack(anchor=E, ipadx=30, pady=30, fill=Y, side=LEFT)
         self.buttonRecognize.pack(side=TOP)
         self.buttonTeach.pack(side=TOP)
+        self.buttonSave.pack(side=TOP)
         self.buttonError.pack(side=TOP)
         self.buttonClear.pack(side=TOP)
         self.lbl1.pack(side=TOP)
@@ -64,4 +64,4 @@ class Window:
 
     def clear(self):
         self.cv.delete("all")
-        self.draw.rectangle((0, 0, Cwidth, Cheight), fill=(255, 255, 255, 255))
+        self.draw.rectangle((0, 0, self.Cwidth, self.Cheight), fill=(255, 255, 255, 255))
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+
 if __name__ == "__main__":
     from Set_UI import Window
     window = Window(350, 250, "Лабораторная работа №0")
