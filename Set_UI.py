130a02c1df37d0db22494e0da19fb6d232760268
IsSabuhi <sabuhi.israfilov2001@gmail.com>
2022-03-05 13:07:58 +0700

    save recog

--- Set_UI.py
+++ Set_UI.py
@@ -16,15 +16,15 @@ class Window:
         self.Cwidth = 80
         self.Cheight = 80
         self.left_frame = Frame(self.root)
-        self.right_frame = Frame(self.root, width=self.Cwidth, height=self.Cheight)
-        self.cv = Canvas(self.right_frame, width=self.Cwidth, height=self.Cheight, bg='white')
-        self.image = PIL.Image.new("RGB", (width, height), self.white)
+        self.cv = Canvas(width=self.Cwidth, height=self.Cheight, bg='white')
+        self.image = PIL.Image.new("RGB", (self.Cwidth, self.Cheight), self.white)
         self.draw = ImageDraw.Draw(self.image)
-        self.penSize_slider = 3
+        self.penSize_slider = 4
         self.buttonRecognize = Button(self.left_frame, text="Распознать", command=self.recognize_event, width=20)
-        self.buttonTeach = Button(self.left_frame, text="Обучить", width=20)
+        self.buttonX = Button(self.left_frame, text="Крест", command=self.button_X_event, width=20)
+        self.buttonO = Button(self.left_frame, text="Нолик", command=self.button_O_event, width=20)
+        self.buttonTeach = Button(self.left_frame, text="Обучение" )
         self.buttonSave = Button(self.left_frame, text="Сохранить веса", command=self.save_weights_event, width=20)
-        self.buttonError = Button(self.left_frame, text="Ошибка", width=20)
         self.buttonClear = Button(self.left_frame, text="Очистить", command=self.clear, width=20)
         self.lbl0 = Label(self.left_frame, text="Размер пера", font="Arial 10", width=15)
         self.lbl1 = Label(self.left_frame, text=" ", font="Arial 10", fg="black")
@@ -34,20 +34,25 @@ class Window:
 
 
     def recognize_event(self):
-        self.rec.recognize(self.image, self.lbl1, self.lbl2, self.Cwidth, self.Cheight)
+        self.rec.recognize(self.image, self.lbl1, self.lbl2)
+
+    def button_X_event(self):
+        self.rec.btn_X()
+
+    def button_O_event(self):
+        self.rec.btn_O()
 
     def save_weights_event(self):
         self.rec.save_w()
 
     def draw_widgets(self):
         self.cv.bind("<B1-Motion>", self.paint)
-        self.cv.pack(pady=60)
+        self.cv.pack(side=RIGHT, pady=60, padx=50)
         self.left_frame.pack(anchor=W, ipadx=20, pady=30, side=LEFT)
-        self.right_frame.pack(anchor=E, ipadx=30, pady=30, fill=Y, side=LEFT)
         self.buttonRecognize.pack(side=TOP)
-        self.buttonTeach.pack(side=TOP)
+        self.buttonX.pack(side=TOP)
+        self.buttonO.pack(side=TOP)
         self.buttonSave.pack(side=TOP)
-        self.buttonError.pack(side=TOP)
         self.buttonClear.pack(side=TOP)
         self.lbl1.pack(side=TOP)
         self.lbl2.pack(side=TOP)
--- main.py
+++ main.py
@@ -2,5 +2,5 @@
 
 if __name__ == "__main__":
     from Set_UI import Window
-    window = Window(350, 250, "Лабораторная работа №0")
+    window = Window(350, 350, "Лабораторная работа №0")
     window.run()
